7966a4e6f0b983f16a6568c0b549acfaf9d2dbb9
徐文海 <xuwenhai@test.com>
2024-11-09 16:49:51 +0800

    feat(demo-vue): 更新 DemoTest 组件并添加 Todos 功能
    - 将 DemoTest 组件从 HelloWorld 组件中解耦
    - 实现了一个简单的 Todos 应用，包括添加和删除任务的功能
    - 优化了组件的样式和布局

--- base/demo-vue/src/App.vue
+++ base/demo-vue/src/App.vue
@@ -9,7 +9,7 @@
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-import DemoTest from "./components/HelloWorld.vue";
+import DemoTest from "./components/DemoTest.vue";
 
 export default {
   name: "App",
--- base/demo-vue/src/components/DemoTest.vue
+++ base/demo-vue/src/components/DemoTest.vue
@@ -1,14 +1,46 @@
 <template>
   <div>
-    <div>{{ msg }}</div>
+   <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 
-    11
 
   </div>
+
+
 </template>
 
 <script>
-export default {};
+let id = 0
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        { id: id++, text: 'Learn HTML' },
+        { id: id++, text: 'Learn JavaScript' },
+        { id: id++, text: 'Learn Vue' }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      // ...
+      this.newTodo = ''
+    },
+
+    removeTodo() {
+      // ...
+    }
+  }
+};
 </script>
 
 <style>
